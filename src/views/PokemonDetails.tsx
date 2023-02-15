494eaeb3beae52c38185b2a3f048882c54201d8f
“IvanMtz97” <ivanmartinez.gonzalez97@gmail.com>
2023-02-15 13:32:47 -0600

    Adding basic routing

---
+++ src/views/PokemonDetails.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PokemonDetails() {
+  return (
+    <div>
+      <span>Pokemon Details</span>
+    </div>
+  );
+}
+
+export default PokemonDetails;
---
+++ src/views/PokemonsList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PokemonsList() {
+  return (
+    <div>
+      <span>PokemonsList</span>
+    </div>
+  );
+}
+
+export default PokemonsList;
